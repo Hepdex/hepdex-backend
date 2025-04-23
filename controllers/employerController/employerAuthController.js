adf0a8ebb3bbd87cc678b5e09af215b26b5f21de
Adriel Amadi <adrielamadi7@gmail.com>
2025-04-23 16:32:52 +0100

    this is the first commit

---
+++ controllers/employerController/employerAuthController.js
@@ -0,0 +1,61 @@
+const database = require("../../lib/database")
+const utilities = require("../../lib/utilities")
+const {ObjectId} = require("mongodb")
+
+const employerAuthController = {}
+
+
+employerAuthController.signup = ("/employer-signup", async (req, res)=>{
+    try {
+        const payload = JSON.parse(req.body)
+
+        //Validate payload
+        const paylodStatus = await utilities.userSignupValidator(payload, ["firstName", "lastName", "email", "companySize", "country", "password"])
+        if(!paylodStatus.isValid){
+            utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {msg: paylodStatus.msg}, true)
+            return
+        }
+
+        //convert email and username to all lowercase
+        payload.email = payload.email.toLowerCase()
+        payload.country = payload.country.toLowerCase()
+        //check if email
+        const uniqueChecker = await database.checkForExistingUser(payload)
+
+
+        if(uniqueChecker.doesUserDetailExist){
+            utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {msg: `this ${uniqueChecker.userDetail} already exists`}, true)
+            return
+        }
+        //hash password
+        payload.password = utilities.dataHasher(payload.password)
+
+        //add other properties
+        payload.role = "employer"
+        payload.createdAt = new Date()
+        payload.isEmailVerified = true //THIS SHOULD BE CHANGED TO FALSE ON PODUCTION
+
+        //generate otp
+        payload.otp = utilities.otpGenerator() 
+
+        //save to database
+        const savedEmployer = await database.insertOne(payload, database.collections.users)
+
+        //send response
+        utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {userID: savedEmployer.insertedId}, true)
+
+        //SEND EMAIL HERE
+
+        return
+            
+        
+    } 
+    catch (err) {
+        console.log(err)    
+        utilities.setResponseData(res, 500, {'content-type': 'application/json'}, {msg: "server error"}, true)
+        return
+    }
+})
+
+
+module.exports = employerAuthController
---
+++ controllers/userController/userAuthController.js
@@ -0,0 +1,95 @@
+const database = require("../../lib/database")
+const utilities = require("../../lib/utilities")
+
+const userAuthController = {}
+
+userAuthController.login = ("/user/login", async (req, res)=>{
+    try {
+        const payload = JSON.parse(req.body)
+
+        // check if user exists
+        const user = await database.findOne({email: payload.email}, database.collections.users)
+
+        if(!user){
+            utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {msg: "invalid email or password"}, true)
+            return
+        }
+
+        //check if email is verified
+        if(!user.isEmailVerified){
+            //generate new OTP
+            const otp = utilities.otpGenerator()
+            //update user object
+            await database.updateOne({_id: user._id}, database.collections.users, {otp})
+            //send response
+            utilities.setResponseData(res, 401, {'content-type': 'application/json'}, {msg: "Unverified email", userId: user._id}, true)
+            //SEND OTP TO EMAIL
+            
+            return
+        }
+        //hash password
+        payload.password = utilities.dataHasher(payload.password)
+        //check if password match
+        if(payload.password !== user.password){
+            utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {msg: "invalid email or password"}, true)
+            return
+        }
+
+        //create token
+        const token = utilities.jwt("s", {userID: user._id, role: user.role})
+        delete user.password
+        delete user.otp
+
+        // Set the token in an HTTP-only cookie
+        res.cookie("token", token, {
+            httpOnly: true,           
+            secure: process.env.NODE_ENV === "production", 
+            sameSite: "Strict",       // Helps protect against CSRF
+            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
+            path: "/"
+        });
+            
+        //send response
+        utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {user}, true)
+        return
+        
+    } 
+    catch (err) {
+        console.log(err)    
+        utilities.setResponseData(res, 500, {'content-type': 'application/json'}, {msg: "server error"}, true)
+        return
+    }
+})
+
+
+userAuthController.logout = ("/logout", async (req, res)=>{
+    try {
+        // Clear the token cookie by setting it with an expired date
+        res.clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "Strict",
+          path: "/"
+        });
+    
+        // Send a success response
+        utilities.setResponseData(
+          res,
+          200,
+          { 'content-type': 'application/json' },
+          { msg: "Logout successful" },
+          true
+        );
+    } catch (err) {
+        console.error(err);
+        utilities.setResponseData(
+          res,
+          500,
+          { 'content-type': 'application/json' },
+          { msg: "Server error" },
+          true
+        );
+    }
+})
+
+module.exports = userAuthController
