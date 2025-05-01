2fa4476b365d0859c93e202d069d86e495ed5a15
Adriel Amadi <adrielamadi7@gmail.com>
2025-05-01 12:57:50 +0100

    completed authentication

--- controllers/candidateController/candidateAuthController.js
+++ controllers/candidateController/candidateAuthController.js
@@ -1,28 +1,32 @@
 const database = require("../../lib/database")
 const utilities = require("../../lib/utilities")
+const { uploadFileToS3 } = require("../../lib/s3Uploader")
+const path = require("path");
 const {ObjectId} = require("mongodb")
-
-const employerAuthController = {}
-
-
-employerAuthController.signup = ("/employer-signup", async (req, res)=>{
-    try {
-        const payload = JSON.parse(req.body)
-
+ 
+const candidateAuthController = {}
+ 
+ 
+candidateAuthController.signup = ("/candidate-signup", async (req, res)=>{
+    try{
+        const payload = req.body
+ 
         //Validate payload
-        const paylodStatus = await utilities.userSignupValidator(payload, ["firstName", "lastName", "email", "companySize", "country", "password"], "employer")
+        const paylodStatus = await utilities.userSignupValidator(payload, ["firstName", "lastName", "email", "jobType", "country", "password"], "candidate")
         if(!paylodStatus.isValid){
             utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {msg: paylodStatus.msg}, true)
             return
         }
+        payload._id = req.generatedUserID
 
+        
         //convert email and username to all lowercase
         payload.email = payload.email.toLowerCase()
         payload.country = payload.country.toLowerCase()
         //check if email
         const uniqueChecker = await database.checkForExistingUser(payload)
-
-
+ 
+ 
         if(uniqueChecker.doesUserDetailExist){
             utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {msg: `this ${uniqueChecker.userDetail} already exists`}, true)
             return
@@ -30,25 +34,38 @@ employerAuthController.signup = ("/employer-signup", async (req, res)=>{
         //hash password
         payload.password = utilities.dataHasher(payload.password)
 
+        if (req.file) {
+            const fileName = `doc-${req.generatedUserID.toString()}-${req.generatedFileId.toString()}${path.extname(req.file.originalname)}`;
+        
+            const s3UploadResult = await uploadFileToS3({
+                buffer: req.file.buffer,
+                fileName: `resumes/${fileName}`, // saves inside 'resumes' folder in bucket
+                mimeType: req.file.mimetype
+            });
+        
+            payload.resumePath = s3UploadResult.Location; // or s3UploadResult.Key if you prefer
+        }
+ 
         //add other properties
-        payload.role = "employer"
+        payload.role = "candidate"
         payload.createdAt = new Date()
         payload.isEmailVerified = false //THIS SHOULD BE CHANGED TO FALSE ON PODUCTION
-
+ 
         //generate otp
         payload.otp = utilities.otpGenerator() 
-
+        console.log(payload)
+ 
         //save to database
-        const savedEmployer = await database.insertOne(payload, database.collections.users)
-
+        const savedCandidate = await database.insertOne(payload, database.collections.users)
+ 
         //send response
-        utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {userID: savedEmployer.insertedId}, true)
-
+        utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {userID: savedCandidate.insertedId}, true)
+ 
         //SEND EMAIL HERE
-
+ 
         return
-            
-        
+             
+         
     } 
     catch (err) {
         console.log(err)    
@@ -56,6 +73,6 @@ employerAuthController.signup = ("/employer-signup", async (req, res)=>{
         return
     }
 })
-
-
-module.exports = employerAuthController
+ 
+ 
+module.exports = candidateAuthController
