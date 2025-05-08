bd615bc5e338835aaec38e86664c76613c51d2d4
Adriel Amadi <adrielamadi7@gmail.com>
2025-05-08 17:37:54 +0100

    final token fix

--- controllers/userController/userAuthController.js
+++ controllers/userController/userAuthController.js
@@ -48,7 +48,7 @@ userAuthController.login = ("/user/login", async (req, res)=>{
             secure: process.env.NODE_ENV === "production", 
             sameSite: "None",       // Helps protect against CSRF
             maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
-            //path: "/"
+            path: "/"
         });
             
         //send response
