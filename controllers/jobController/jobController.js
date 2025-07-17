889fadc8141c3839bfff809f8590993d1c68d69a
Adriel Amadi <adrielamadi7@gmail.com>
2025-07-17 21:30:48 +0100

    made changes on sharing jobs to socials

--- controllers/jobController/jobController.js
+++ controllers/jobController/jobController.js
@@ -706,7 +706,7 @@ jobController.getJobSharePage = async (req, res) => {
 
         const jobTitle = escape(job.jobTitle) || "Untitled Job";
         const jobDesc = escape(job.aboutRole?.slice(0, 200) ) || "View this opportunity on Hepdex.";
-        const ogImage = escape(job.employerDetails.companyLogo)  || "https://api.hepdex.com/images/logo2.jpg"; // Optional fallback image
+        const ogImage = "https://api.hepdex.com/images/share-job-img.jpg";    //escape(job.employerDetails.companyLogo)  || "https://api.hepdex.com/images/logo2.jpg"; 
         const companyName = job.employerDetails.companyName ? job.employerDetails.companyName.replace(/\s+/g, "-") : "Hepdex";
         const jobUrl = `https://hepdex.com/jobs/${companyName}/${slug}`;
 
